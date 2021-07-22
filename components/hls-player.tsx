7db7dd322d3456845385b846d0f80a1ab25d2107
Marcel Fahle <m.fahle@gmail.com>
2021-07-22 11:32:22 +0300

    initial commmit

---
+++ components/hls-player.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import Hls from "hls.js";
+
+export function HlsPlayer() {
+  const videoEl = React.useRef(null);
+
+  React.useEffect(() => {
+    // var video = document.getElementById('video');
+    const video = videoEl.current;
+    if (Hls.isSupported()) {
+      var hls = new Hls({
+        debug: true
+      });
+      hls.loadSource(
+        "https://stream.mux.com/k02s1qHF0102S6vHADc00z1wCoICAsSbUOm9.m3u8"
+      );
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MEDIA_ATTACHED, function() {
+        video.muted = true;
+        video.play();
+      });
+    }
+    // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+    // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+    // This is using the built-in support of the plain video element, without using hls.js.
+    else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src =
+        "https://stream.mux.com/k02s1qHF0102S6vHADc00z1wCoICAsSbUOm9.m3u8";
+      video.addEventListener("canplay", function() {
+        video.play();
+      });
+    }
+  }, []);
+
+  return <video height="600" id="video" ref={videoEl} controls></video>;
+}
---
+++ pages/speedtest1.tsx
@@ -0,0 +1,10 @@
+import { HlsPlayer } from "../components/hls-player";
+function Speedtest1() {
+  return (
+    <div>
+      <HlsPlayer />
+    </div>
+  );
+}
+
+export default Speedtest1;
